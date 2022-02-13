ac3ac59f4c48113c41c53c276f254d1a0c8ad39a
Felix <felix.zenk@web.de>
2022-02-13 18:35:41 +0100

    Implemented fmt

--- src/bexp.rs
+++ src/bexp.rs
@@ -7,12 +7,26 @@ use crate::aexp::*;
 #[derive(PartialEq,Clone,Debug,Serialize,Deserialize,Eq,Hash)]
 pub enum BExp {
     LessEq(Box<AExp>, Box<AExp>),
+    Conjunction(Box<BExp>, Box<BExp>),
+    Disjunction(Box<BExp>, Box<BExp>),
+    Negation,
 }
 
 impl Display for BExp {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match &self {
-            BExp::LessEq(left, right) => {write!(f, "{} <= {}", left, right)}
+            BExp::LessEq(left, right) => {
+                write!(f, "{} <= {}", left, right)
+            }
+            BExp::Conjunction(left, right) => {
+                write!(f, "{} and {}", left, right)
+            }
+            BExp::Disjunction(left, right) => {
+                write!(f, "({} or {})", left, right)
+            }
+            BExp::Negation => {
+                write!(f, "not {}", self)
+            }
         }
     }
 }
@@ -23,6 +37,15 @@ impl BExp {
             BExp::LessEq(a1, a2) => {
                 // Rust Expl.: See also `AExp::sub_aexps` for a more detailed explanation 
                 a1.sub_aexps().union(&a2.sub_aexps()).cloned().collect()
+            }
+            BExp::Conjunction(a1, a2) => {
+                // a1.sub_aexps().union(&a2.sub_aexps()).cloned().collect()
+            }
+            BExp::Disjunction(a1, a2) => {
+
+            }
+            BExp::Negation => {
+                // Negation of self.sub_aexps().clone()
             }
         }
     }
